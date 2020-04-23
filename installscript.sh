b23d790be76e5a6c0ab00339dfabbcee23ba6d14
1ngr1dmd <64015064+1ngr1dmd@users.noreply.github.com>
2020-04-23 11:18:12 +0200

    m

--- installscript.sh
+++ installscript.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-echo "Copy required files to RP"
+echo Copy required files to RP
 
-scp rp_files/RPServer.py rp_files/SimonLab_FIRVNA.bit rp_files/JSocket.py root@$1:~
+scp rp_files/RPServer.py root@$1:~
+scp rp_files/SimonLab_FIRVNA.bit root@$1:~
+scp rp_files/JSocket.py root@$1:~
 scp rp_files/rc.local root@$1:/etc/rc.local
 scp rp_files/FPGAreporter.py root@$1:/usr/bin/FPGAreporter.py
 
 ssh root@$1 systemctl disable redpitaya_nginx
 #ssh root@$1 systemctl disable redpitaya_wyliodrin
 ssh root@$1 systemctl disable redpitaya_scpi
-ssh root@$1 nohup python RPServer.py </dev/null >/dev/null 2>&1 &
-ssh root@$1 nohup python /usr/bin/FPGAreporter.py </dev/null >/dev/null 2>&1 &
+ssh root@$1 nohup python3 RPServer.py </dev/null >/dev/null 2>&1 &
+ssh root@$1 nohup python3 /usr/bin/FPGAreporter.py </dev/null >/dev/null 2>&1 &
