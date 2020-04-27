51dc2fb003def4941e11fa985c1eabc7d676be74
1ngr1dmd <64015064+1ngr1dmd@users.noreply.github.com>
2020-04-27 15:39:42 +0200

    edit readme

--- FPGAreporter.py
+++ FPGAreporter.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import time
 import datetime
 from time import sleep
--- installscript.sh
+++ installscript.sh
@@ -5,8 +5,9 @@ scp RPServer.py SimonLab_MDDS.bit JSocket.py root@$1:~
 scp rc.local root@$1:/etc/rc.local
 scp FPGAreporter.py root@$1:/usr/bin/FPGAreporter.py
 
-ssh root@$1 "systemctl disable redpitaya_nginx"
-ssh root@$1 "systemctl disable redpitaya_wyliodrin"
-ssh root@$1 "systemctl disable redpitaya_scpi"
-ssh root@$1 "nohup python3 RPServer.py &"
-ssh root@$1 "nohup python3 /usr/bin/FPGAreporter.py &"
+ssh root@$1 "systemctl disable redpitaya_nginx; systemctl disable redpitaya_scpi"
+
+ssh root@$1 "chmod +x RPServer.py; chmod +x /usr/bin/FPGAreporter.py"
+
+ssh root@$1 "nohup python3 RPServer.py < /dev/null > /dev/null 2>&1 &"
+ssh root@$1 "nohup python3 /usr/bin/FPGAreporter.py < /dev/null > /dev/null 2>&1 &"
