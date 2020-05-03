8daf24a12bff6e8f5d7832c2344fcadaf975a556
1ngr1dmd <64015064+1ngr1dmd@users.noreply.github.com>
2020-05-03 07:55:05 +0200

    final testing

--- installscript.sh
+++ installscript.sh
@@ -2,9 +2,10 @@
 echo "--- COPY ALL NECESSARY FILES; DISABLE SOME FEATURES; START SERVER ON RP ---"
 
 scp RPServer.py SimonLab_MDDS.bit JSocket.py root@$1:~
+scp rc.local root@$1:/etc/rc.local
 
 #ssh root@$1 "systemctl disable redpitaya_nginx"
 #ssh root@$1 "systemctl disable redpitaya_scpi"
 #ssh root@$1 "systemctl disable jupyter"
 
-ssh root@$1 "nohup python3 /home/RPServer.py > foo.out 2> foo.err < /dev/null &"
+ssh root@$1 "nohup python3 ~/RPServer.py < /dev/null > /dev/null 2>&1 &"
