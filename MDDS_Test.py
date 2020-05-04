05c6cc99f8e7e698f23db33e61db0a0f0ab6c61c
1ngr1dmd <64015064+1ngr1dmd@users.noreply.github.com>
2020-05-04 16:31:16 +0200

    Update MDDS_Test.py

--- MDDS_Test.py
+++ MDDS_Test.py
@@ -10,16 +10,16 @@ REDPITAYA_IP = "10.22.1.178" #input RP's IP to run procedure
 SOFTWARETRIGGER=1
 #the next line of code generates the data for each of the 10 simultaneous DDS outputs. The format, for each channel, is:
 #[[f_initial in Hz, Amp_initial as fraction of max amplitude],[[time of end of first ramp, freq to ramp to, amplitude to ramp to],[time of end of second ramp, freq to ramp to, amplitude to ramp to],...]]
-CHs_DATA=[[     [50e6, 1.00], [[4.0, 50e6, 1.00]], #ch0
-                [10e6, 0.00], [[4.0, 10e6, 0.00]], #ch1
-                [15e6, 0.00], [[4.0, 15e6, 0.00]], #ch2
-                [20e6, 0.00], [[4.0, 20e6, 0.00]], #ch3
-                [25e6, 0.00], [[4.0, 25e6, 0.00]], #ch4
-                [30e6, 0.00], [[4.0, 30e6, 0.00]], #ch5
-                [35e6, 0.00], [[4.0, 35e6, 0.00]], #ch6
-                [40e6, 0.00], [[4.0, 40e6, 0.00]], #ch7
-                [45e6, 0.00], [[4.0, 45e6, 0.00]], #ch8
-                [50e6, 0.00], [[4.0, 50e6, 0.00]]  #ch9
+CHs_DATA=[[     [5e6, 1.00], [[0.0, 5e6, 1.00]], #ch0
+                [10e6, 0.00], [[0.0, 10e6, 0.00]], #ch1
+                [15e6, 0.00], [[0.0, 15e6, 0.00]], #ch2
+                [20e6, 0.00], [[0.0, 20e6, 0.00]], #ch3
+                [25e6, 0.00], [[0.0, 25e6, 0.00]], #ch4
+                [30e6, 0.00], [[0.0, 30e6, 0.00]], #ch5
+                [35e6, 0.00], [[0.0, 35e6, 0.00]], #ch6
+                [40e6, 0.00], [[0.0, 40e6, 0.00]], #ch7
+                [45e6, 0.00], [[0.0, 45e6, 0.00]], #ch8
+                [50e6, 0.00], [[0.0, 50e6, 0.00]]  #ch9
                     ]]
 
 DDSMulti_Sequencer.SendDataToRP(REDPITAYA_IP, SOFTWARETRIGGER, CHs_DATA)
